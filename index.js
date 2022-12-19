1ce491e7480d97cf9add9923ac208a0ae772b577
Ricahrdokkkkk <anoquos@gmail.com>
2022-12-19 17:03:40 -0500

    Homepage update

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ setInterval(function () {
     if (window.scrollY <= 500) {
         document.getElementById("titlebox").style = "height: 200px"; 
     }
-    if (window.scrollY>= 500 && window.scrollY <=1750) {
+    if (window.scrollY>= 500) {
         document.getElementById("titlebox").style = "height: 275vh"; 
-    }
-    if (window.scrollY>= 1750 && window.scrollY <=3000) {
-        document.getElementById("titlebox").style = "height: 620vh"; 
     }
 
 }, 10)
 
+$(function() {
+    $('#contentbutton1').hover(function() {
+      $('#sec3').css('background', 'linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 89, 167, 0.8)), url(./images/earth.jpg)');
+    }, function() {
+      // on mouseout, reset the background colour
+      $('#sec3').css('background', 'linear-gradient(rgba(0, 43, 161, 0.2), var(--main)), url(./mages/earth.jpg)');
+    });
+  });
 
