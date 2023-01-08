f5c90a3238dae086dcb0b3c3ca5e44083eb18cc5
Richard Lin <richardlin4375@gmail.com>
2023-01-07 23:08:03 -0500

    New mobile menu, vehicles page, about us page rework

---
+++ universal.js
@@ -0,0 +1,45 @@
+let width = window.screen.width;
+if (width > 1024) {
+    setInterval(function () {
+    if (window.scrollY <= 100) {
+        document.getElementById("menu").style = "background-color: transparent";
+    }
+    else {
+        document.getElementById("menu").style = "background-color: var(--main)";
+    }
+}, 10)
+}
+
+/*menu*/
+var openm = false;
+function openmenu() {
+    if (openm == false) {
+        document.getElementById("menu").style.height = "1900px";
+        openm = true;
+        document.getElementById("menubutton").style.transform = "rotate(-90deg)";
+    }
+    else if (openm == true) {
+        document.getElementById("menu").style.height = "170px";
+        openm = false;
+        document.getElementById("menubutton").style.transform = "rotate(90deg)";
+    }
+}
+
+setInterval(function() {
+    let width = window.screen.width;
+    if (width < 1024 && openm == true) {
+        document.getElementById("menu").style.height = "1900px";
+        document.getElementById("menu").style.backgroundColor = "var(--main)";
+    }
+    else if (width > 1024) {
+        document.getElementById("menu").style.height = "85px";
+    }
+
+}, 10);
+
+document.getElementById("contactlink").onmouseover = function() {
+    document.getElementById("contactlink").style.cursor = "pointer";
+}
+document.getElementById("contactlink").onclick = function() {
+    window.scrollTo({top: 10000, behavior: 'smooth'});
+}   
