05053b8b6d03550aebaadc28a8b63897daaf7ab8
Richard Lin <richardlin4375@gmail.com>
2023-02-09 20:48:01 -0500

    Final changes and info update

--- travel.js
+++ travel.js
@@ -2,6 +2,7 @@ var c1 = 0;
 var c2 = 0;
 var c3 = 0;
 var c4 = 0;
+var c5 = 0;
 
 document.getElementById("qa1").style = "height: 0px; padding-top: 0; padding-bottom: 0; padding-left: 50px; padding-right: 50px; border: 0;";
 document.getElementById("q1").style = "height: 0; display: none;";
@@ -14,6 +15,9 @@ document.getElementById("q3").style = "height: 0; display: none;";
 
 document.getElementById("qa4").style = "height: 0px; padding-top: 0; padding-bottom: 0; padding-left: 50px; padding-right: 50px; border: 0;";
 document.getElementById("q4").style = "height: 0; display: none;";
+
+document.getElementById("qa5").style = "height: 0px; padding-top: 0; padding-bottom: 0; padding-left: 50px; padding-right: 50px; border: 0;";
+document.getElementById("q5").style = "height: 0; display: none;";
 
 document.getElementById("tripheader").onmousedown = function(){
         if (c1 % 2 == 0) {
@@ -34,11 +38,11 @@ document.getElementById("tripheader").onmousedown = function(){
 
 document.getElementById("roomheader").onmousedown = function(){
     if (c2 % 2 == 0) {
-        document.getElementById("qa2").style = "height: 200px;padding-top: 50px;padding-left: 50px;padding-right: 50px;padding-bottom: 50px;border-bottom: 6px solid var(--third);border-right: 6px solid var(--third);";
-        document.getElementById("q2").style = "height: 200px";
+        document.getElementById("qa2").style = "height: 550px;padding-top: 50px;padding-left: 50px;padding-right: 50px;padding-bottom: 50px;border-bottom: 6px solid var(--third);border-right: 6px solid var(--third);";
+        document.getElementById("q2").style = "height: 550px";
         if (window.innerWidth < 1024) {
-            document.getElementById("q2").style = "height: 900px; width: 100%; padding-top: 50px;padding-left: 50px;padding-right: 50px;padding-bottom: 50px;border-bottom: 6px solid var(--third);border-right: 6px solid var(--third);";
-            document.getElementById("qa2").style = "height: 900px";
+            document.getElementById("q2").style = "height: 1400px; width: 100%; padding-top: 50px;padding-left: 50px;padding-right: 50px;padding-bottom: 50px;border-bottom: 6px solid var(--third);border-right: 6px solid var(--third);";
+            document.getElementById("qa2").style = "height: 1400px";
         }
         c2++;
     } 
@@ -66,13 +70,13 @@ document.getElementById("activityheader").onmousedown = function(){
     }
 };
 
-document.getElementById("foodheader").onmousedown = function(){
+document.getElementById("planetsheader").onmousedown = function(){
     if (c4 % 2 == 0) {
-        document.getElementById("qa4").style = "height: 400px;padding-top: 50px;padding-left: 50px;padding-right: 50px;padding-bottom: 50px;border-bottom: 6px solid var(--third);border-right: 6px solid var(--third);";
-        document.getElementById("q4").style = "height: 400px";
+        document.getElementById("qa4").style = "height: 1150px;padding-top: 50px;padding-left: 50px;padding-right: 50px;padding-bottom: 50px;border-bottom: 6px solid var(--third);border-right: 6px solid var(--third);";
+        document.getElementById("q4").style = "height: 1150px";
         if (window.innerWidth < 1024) {
-            document.getElementById("q4").style = "height: 1700px; width: 100%; padding-top: 50px;padding-left: 50px;padding-right: 50px;padding-bottom: 50px;border-bottom: 6px solid var(--third);border-right: 6px solid var(--third);";
-            document.getElementById("qa4").style = "height: 1700px";
+            document.getElementById("q4").style = "height: 4250px; width: 100%; padding-top: 50px;padding-left: 50px;padding-right: 50px;padding-bottom: 50px;border-bottom: 6px solid var(--third);border-right: 6px solid var(--third);";
+            document.getElementById("qa4").style = "height: 4250px";
         }
         c4++;
     } 
@@ -80,5 +84,22 @@ document.getElementById("foodheader").onmousedown = function(){
         document.getElementById("qa4").style = "height: 0px; padding-top: 0; padding-bottom: 0; border: 0;";
         document.getElementById("q4").style = "height: 0; display: none;";
         c4++;
+    }
+};
+
+document.getElementById("foodheader").onmousedown = function(){
+    if (c5 % 2 == 0) {
+        document.getElementById("qa5").style = "height: 750px;padding-top: 50px;padding-left: 50px;padding-right: 50px;padding-bottom: 50px;border-bottom: 6px solid var(--third);border-right: 6px solid var(--third);";
+        document.getElementById("q5").style = "height: 750px";
+        if (window.innerWidth < 1024) {
+            document.getElementById("q5").style = "height: 2800px; width: 100%; padding-top: 50px;padding-left: 50px;padding-right: 50px;padding-bottom: 50px;border-bottom: 6px solid var(--third);border-right: 6px solid var(--third);";
+            document.getElementById("qa5").style = "height: 2800px";
+        }
+        c5++;
+    } 
+    else {
+        document.getElementById("qa5").style = "height: 0px; padding-top: 0; padding-bottom: 0; border: 0;";
+        document.getElementById("q5").style = "height: 0; display: none;";
+        c5++;
     }
 };
