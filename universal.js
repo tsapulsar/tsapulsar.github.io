a56d8585de98e161f80b5a21097db03a61ef38e5
Richard Lin <richardlin4375@gmail.com>
2023-02-09 21:33:33 -0500

    Finalized

--- universal.js
+++ universal.js
@@ -41,7 +41,7 @@ document.getElementById("contactlink").onmouseover = function() {
     document.getElementById("contactlink").style.cursor = "pointer";
 }
 document.getElementById("contactlink").onclick = function() {
-    window.scrollTo({top: 1000000000000000, behavior: 'smooth'});
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
     if (width < 1024 && openm == true) {
         openm = false;
         document.getElementById("menubutton").style.transform = "rotate(90deg)";
