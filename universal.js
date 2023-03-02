c863cc3180c64a8dd2b5b306f72d8dba915a8c48
Richard Lin <richardlin4375@gmail.com>
2023-03-01 23:25:36 -0500

    Page Transition, book/login buttons, fixed contact footer, made additional menu changes

--- universal.js
+++ universal.js
@@ -1,3 +1,128 @@
+
+(function () {
+  var div = document.createElement('div'),
+      ref = document.getElementsByTagName('base')[0] || 
+            document.getElementsByTagName('script')[0];
+  div.innerHTML = '&shy;<style> iframe { visibility: hidden; } </style>';
+  ref.parentNode.insertBefore(div, ref);
+  window.onload = function() {
+        window.scrollTo(0, 0);
+        div.parentNode.removeChild(div);
+        document.getElementById("paget").style = "width:0";
+        document.getElementById("pagettext").style = "opacity: 0;";
+        let headerMain = `
+        <nav>
+            <div id="menu">
+            <div id="menucontent">
+                <p><p id="username">PULSAR</p></p>
+                <button id="menubutton" onclick="openmenu()">></button>
+            </div>
+            <div id="header-main">
+                <p class="menu-link" id="homeb">Home</p>
+                <p class="menu-link" id="aboutb">About</p>
+                <p class="menu-link" id="travelb">Travel</p>
+                <p class="menu-link" id="safetyb">Safety</p>
+                <p class="menu-link" id="vehiclesb">Vehicles</p>
+                <p class="menu-link" id="visitb">Visit</p>
+                <p class="menu-link" id="refb">References</p>
+                <p class="menu-link" id="contactlink">Contact</p>
+            </div>
+            <div id="menu-right">
+                <a href="./book.html" id="rightbutton1">Book</a>
+                <a href="./login.html" id="rightbutton2">Login</a>
+            </div>
+                <script async src="universal.js"></script> 
+            </div>
+        </nav>
+        `;
+        document.getElementById("menu-main").innerHTML = headerMain;
+        document.getElementById("homeb").onmousedown = function(){
+            document.getElementById("paget").style = "left: 0; width:100%";
+            document.getElementById("pagettext").style = "opacity: 1";
+            setTimeout(function(){ window.location.href = "./index.html"; }, animateSpeed);
+        };
+        document.getElementById("aboutb").onmousedown = function(){
+            document.getElementById("paget").style = "left: 0; width:100%";
+            document.getElementById("pagettext").style = "opacity: 1";
+            setTimeout(function(){ window.location.href = "./about.html"; }, animateSpeed);
+        };
+        document.getElementById("travelb").onmousedown = function(){
+            document.getElementById("paget").style = "left: 0; width:100%";
+            document.getElementById("pagettext").style = "opacity: 1";
+            setTimeout(function(){ window.location.href = "./travel.html"; }, animateSpeed);
+        };
+        document.getElementById("safetyb").onmousedown = function(){
+            document.getElementById("paget").style = "left: 0; width:100%";
+            document.getElementById("pagettext").style = "opacity: 1";
+            setTimeout(function(){ window.location.href = "./safety.html"; }, animateSpeed);
+        };
+        document.getElementById("vehiclesb").onmousedown = function(){
+            document.getElementById("paget").style = "left: 0; width:100%";
+            document.getElementById("pagettext").style = "opacity: 1";
+            setTimeout(function(){ window.location.href = "./vehicles.html"; }, animateSpeed);
+        };
+        document.getElementById("visitb").onmousedown = function(){
+            document.getElementById("paget").style = "left: 0; width:100%";
+            document.getElementById("pagettext").style = "opacity: 1";
+            setTimeout(function(){ window.location.href = "./visit.html"; }, animateSpeed);
+        };
+        document.getElementById("refb").onmousedown = function(){
+            document.getElementById("paget").style = "left: 0; width:100%";
+            document.getElementById("pagettext").style = "opacity: 1";
+            setTimeout(function(){ window.location.href = "./references.html"; }, animateSpeed);
+        };
+        document.getElementById("rightbutton1").onmousedown = function(){
+            document.getElementById("paget").style = "left: 0; width:100%";
+            document.getElementById("pagettext").style = "opacity: 1";
+            setTimeout(function(){ window.location.href = "./book.html"; }, animateSpeed);
+        };
+        document.getElementById("rightbutton2").onmousedown = function(){
+            document.getElementById("paget").style = "left: 0; width:100%";
+            document.getElementById("pagettext").style = "opacity: 1";
+            setTimeout(function(){ window.location.href = "./login.html"; }, animateSpeed);
+        };
+        let footerMain = `
+        <nav>
+        <div class="contactdetails">
+            <div class="contactdetail">
+                <p id="contactheader">PULSAR</p>
+                <image src="./images/facebook.png" id="social"></image>
+                <image src="./images/twitter.png" style="margin-left: 10px;" id="social"></image>
+                <image src="./images/youtube.png" style="margin-left: 10px;" id="social"></image>
+                <p id="copyright">Copyright © 2140 - 2148 PULSAR</p>
+
+            </div>
+            <div class="contactdetail">
+                <p class="contacttitle">Headquarters Location & <br>Mailing Address</p>
+                <p class="contactinfo">5783 Newport Drive<br>Charlotte NC, 28328</p>
+            </div>
+            <div class="contactdetail">
+                <p class="contacttitle">Questions?</p>
+                <p class="contactinfo"><b>Email:</b> contact@tsapulsar.com<br><b>Phone:</b> 745-473-1365</p>
+            </div>
+        </div>
+
+        </nav>
+    `;
+    document.getElementById("footer-main").innerHTML = footerMain;
+    document.getElementById("footer-main").style.background = "var(--mbg)";
+
+    
+    document.getElementById("contactlink").onmouseover = function() {
+        document.getElementById("contactlink").style.cursor = "pointer";
+    }
+    document.getElementById("contactlink").onclick = function() {
+        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
+        if (width < 1024 && openm == true) {
+            openm = false;
+            document.getElementById("menubutton").style.transform = "rotate(90deg)";
+            document.getElementById("menu").style.height = "180px";
+        }
+    }
+  }
+  
+})();
+
 let width = window.screen.width;
 if (width > 1024) {
     setInterval(function () {
@@ -9,50 +134,17 @@ if (width > 1024) {
     }
 }, 10)
 }
-let headerMain = `
-    <nav>
-        <a href="./index.html" class="menu-link">Home</a>
-        <a href="./about.html" class="menu-link">About</a>
-        <a href="./travel.html" class="menu-link">Travel</a>
-        <a href="./safety.html" class="menu-link">Safety</a>
-        <a href="./vehicles.html" class="menu-link">Vehicles</a>
-        <a href="./visit.html" class="menu-link">Visit</a>
-        <a href="./references.html" class="menu-link">References</a>
-        <a class="menu-link" id="contactlink">Contact</a>
-        <a href="./book.html" class="menu-link" id="right_side" >Book</a>
-        <a href="./login.html" class="menu-link" >Login</a>
-    </nav>
-`;
-document.getElementById("header-main").innerHTML = headerMain;
 
-let footerMain = `
+function wait(ms){
+    var start = new Date().getTime();
+    var end = start;
+    while(end < start + ms) {
+      end = new Date().getTime();
+   }
+ }
 
-    <nav>
-    <div class="contactdetails">
-        <div class="contactdetail">
-            <p id="contactheader">PULSAR</p>
-            <image src="./images/facebook.png" id="social"></image>
-            <image src="./images/twitter.png" style="margin-left: 10px;" id="social"></image>
-            <image src="./images/youtube.png" style="margin-left: 10px;" id="social"></image>
-            <p id="copyright">Copyright © 2140 - 2148 PULSAR</p>
+var animateSpeed = 300;
 
-        </div>
-        <div class="contactdetail">
-            <p class="contacttitle">Headquarters Location & <br>Mailing Address</p>
-            <p class="contactinfo">5783 Newport Drive<br>Charlotte NC, 28328</p>
-        </div>
-        <div class="contactdetail">
-            <p class="contacttitle">Questions?</p>
-            <p class="contactinfo"><b>Email:</b> contact@tsapulsar.com<br><b>Phone:</b> 745-473-1365</p>
-        </div>
-    </div>
-
-    </nav>
-
-
-`;
-
-document.getElementById("footer-main").innerHTML = footerMain;
 /*menu*/
 var openm = false;
 function openmenu() {
@@ -79,15 +171,3 @@ setInterval(function() {
     }
 
 }, 10);
-
-document.getElementById("contactlink").onmouseover = function() {
-    document.getElementById("contactlink").style.cursor = "pointer";
-}
-document.getElementById("contactlink").onclick = function() {
-    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
-    if (width < 1024 && openm == true) {
-        openm = false;
-        document.getElementById("menubutton").style.transform = "rotate(90deg)";
-        document.getElementById("menu").style.height = "180px";
-    }
-}
